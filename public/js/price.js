d0d6cbb33515fe9998edfae0b4d9c4ea19a4517e
matveev_vo <angrucat@gmail.com>
2022-06-07 16:04:16 +0300

    jk

--- public/js/price.js
+++ public/js/price.js
@@ -2,17 +2,24 @@ $(function () {
     $('.price-show').click(function (e) {
         e.preventDefault();
         price = $(this).attr('data-id');
-        if (!$('.price-empty' + price).textContent === '') {
-            $('.price-empty' + price).empty();
-        }
-        else {
+      //  console.log($('.price-empty'+price).text());
+      //  console.log(price.text());
+       // if ($('.price-empty'+price).text() == price)
+      //  $('.price-empty' + price).empty();
             $.ajax({
                 data: 'id=' + price,
                 url: '/ajax/price',
-                success: function (data) {
+                success:
+                    function (data) {
+                    if ($('.price-empty'+price).text() === data){
+                        $('.price-empty' + price).empty();
+                    }else {
                     $('.price-empty'+price).text(data);
+                    console.log(data);
+                    }
                 }
+
             });
-        }
+
     });
 });
