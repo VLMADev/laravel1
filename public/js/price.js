342a960ab3a84fc8e8fd0de259a44661ed1185b7
matveev_vo <angrucat@gmail.com>
2022-06-07 16:04:44 +0300

    jk

--- public/js/price.js
+++ public/js/price.js
@@ -2,10 +2,6 @@ $(function () {
     $('.price-show').click(function (e) {
         e.preventDefault();
         price = $(this).attr('data-id');
-      //  console.log($('.price-empty'+price).text());
-      //  console.log(price.text());
-       // if ($('.price-empty'+price).text() == price)
-      //  $('.price-empty' + price).empty();
             $.ajax({
                 data: 'id=' + price,
                 url: '/ajax/price',
@@ -15,7 +11,6 @@ $(function () {
                         $('.price-empty' + price).empty();
                     }else {
                     $('.price-empty'+price).text(data);
-                    console.log(data);
                     }
                 }
 
